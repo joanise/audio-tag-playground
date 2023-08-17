2072fcd7545f21966cea08170c7e6fd5ec32ab0c
Eric Joanis <eric.joanis@nrc-cnrc.gc.ca>
2023-08-16 22:19:12 -0400

    use pytaglib because it supports all formats, but this probably means GPLv3.0

--- globit.py
+++ globit.py
@@ -1,7 +1,5 @@
-import music_tag
 import re
 import taglib
-from tinytag import TinyTag
 from pathlib import Path
 
 def split_path(path: Path) -> list[str]:
@@ -12,7 +10,7 @@ def split_path(path: Path) -> list[str]:
     else:
         return split_path(path.parent) + [path.name]
 
-FIELD_LIST = ("artist", "album", "tracknumber", "tracktitle")
+FIELD_LIST = ("ARTIST", "ALBUM", "TRACKNUMBER", "TITLE")
 def empty_file_info() -> dict:
     return {f: "" for f in FIELD_LIST}
 
@@ -20,63 +18,55 @@ def infer_from_path(path: Path) -> dict:
     path_elements = split_path(path)
     info = empty_file_info()
     if file.stem[0:2].isdigit() and file.stem[2:3] == " ":
-        info["tracknumber"] = str(int(file.stem[0:2]))
-        info["tracktitle"] = file.stem[3:]
+        info["TRACKNUMBER"] = str(int(file.stem[0:2]))
+        info["TITLE"] = file.stem[3:]
     if len(path_elements) == 4:
-        _, info["artist"], info["album"], _ = path_elements
+        _, info["ARTIST"], info["ALBUM"], _ = path_elements
     return info
-
-def tinytag2musictag(tag):
-    return {
-        "tracknumber": tag.track,
-        "tracktitle": tag.title,
-        "album": tag.album,
-        "artist": tag.artist,
-    }
 
 def looks_like_missing_tags(file_info, tags) -> bool:
     return (
-       str(tags["artist"]) == "Unknown artist" and
-       file_info["artist"] != "" and
-       str(tags["album"]).startswith("Unknown album") and
-       file_info["album"] != "" and
-       re.fullmatch(r"\d\d*", file_info["tracknumber"]) and
-       file_info["tracktitle"] != ""
+       tags["ARTIST"][0] == "Unknown artist" and
+       file_info["ARTIST"] != "" and
+       tags["ALBUM"][0].startswith("Unknown album") and
+       file_info["ALBUM"] != "" and
+       re.fullmatch(r"\d\d*", file_info["TRACKNUMBER"]) and
+       file_info["TITLE"] != ""
     )
     
 def looks_like_only_filename_is_wrong(file_info, tags) -> bool:
     return (
-        str(tags["artist"]) == file_info["artist"] and
-        str(tags["album"]) == file_info["album"] and
-        file_info["tracknumber"] == "" and
-        str(tags["tracknumber"]) != "" and
-        file_info["tracktitle"] == "" and
-        str(tags["tracktitle"]) != ""
+        tags["ARTIST"][0] == file_info["ARTIST"] and
+        tags["ALBUM"][0] == file_info["ALBUM"] and
+        file_info["TRACKNUMBER"] == "" and
+        tags["TRACKNUMBER"][0] != "" and
+        file_info["TITLE"] == "" and
+        tags["TITLE"][0] != ""
     )
 
+def update_tag(song, tag, value):
+    del song.tags[tag]
+    song.tags[tag] = [value]
+
 for file in Path("bad").glob("**/*.*"):
+    if file.name == "desktop.ini": continue
     print(file)
     file_info = infer_from_path(file)
-    try:
-        tags = music_tag.load_file(file)
-        writable_tags = True
-    except NotImplementedError:
-        print(f"{file.suffix} format not supported by music-tag")
-        tags = tinytag2musictag(TinyTag.get(file))
-        writable_tags = False
+    with taglib.File(file) as song:
 
-    for f in FIELD_LIST:
-        if str(file_info[f]) != str(tags[f]):
-            print(f"Mismatched {f}: path: '{file_info[f]}' tags: '{tags[f]}'")
+        for f in FIELD_LIST:
+            if str(file_info[f]) != str(song.tags[f][0]):
+                print(f"Mismatched {f}: path: '{file_info[f]}' tags: '{song.tags[f][0]}'")
 
-    # case 1: info from path looks ok but not info from tags
-    if looks_like_missing_tags(file_info, tags):
-        print(
-            f"==== Action: update tags from filename and path to {file_info}" +
-            ("" if writable_tags else " BUT tags not writable")
-        )
+        # case 1: info from path looks ok but not info from tags
+        if looks_like_missing_tags(file_info, song.tags):
+            print(f"==== Action: update tags from filename and path to {file_info}")
+            print(song.tags)
+            update_tag(song, "TITLE", file_info["TITLE"])
+            update_tag(song, "TRACKNUMBER", file_info["TRACKNUMBER"])
+            print(song.tags)
 
-    # case 2: filename only is wrong
-    elif looks_like_only_filename_is_wrong(file_info, tags):
-        new_file_name = "%02d %s%s" % (int(tags["tracknumber"]), tags["tracktitle"], file.suffix)
-        print(f"==== Action: rename file to '{new_file_name}'")
+        # case 2: filename only is wrong
+        elif looks_like_only_filename_is_wrong(file_info, song.tags):
+            new_file_name = "%02d %s%s" % (int(song.tags["TRACKNUMBER"][0]), song.tags["TITLE"][0], file.suffix)
+            print(f"==== Action: rename file to '{new_file_name}'")
--- scratch.py
+++ scratch.py
@@ -1,10 +1,17 @@
 import music_tag
 tag = music_tag.load_file("mp3/01 Politik.mp3")
 print(tag)
-tag["artist"] = "foo"
-tag.save()
+#tag["artist"] = "foo"
+#tag.save()
 
 import taglib
-with taglib.File("bad/Kevin Parent/Pigeon d'argile/Unknown Artist-Grand Parleur-01-Track 1_[1].wma") as song:
+with taglib.File("bad/Kevin Parent/Pigeon d'argile/Unknown Artist-Grand Parleur-01-Track 1_[1].wma", save_on_exit=True) as song:
     print(song.tags)
-    
+    del song.tags["TITLE"]
+    #song.tags["TITLE"] = ["foo"]
+    song.tags["TITLE"] = "La jasette"
+
+with taglib.File("mp3/01 Politik.mp3", save_on_exit=True) as song:
+    print(song.tags)
+    del song.tags["ARTIST"]
+    song.tags["artist"] = "jack"
